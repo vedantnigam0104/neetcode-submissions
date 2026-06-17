a2064217093b511233917eb6a4ef937e9a459fb3
vedantnigam0104 <105799215+vedantnigam0104@users.noreply.github.com>
2026-06-17 19:46:15 +0530

    Add: permutation-string - submission-0

---
+++ Data Structures & Algorithms/permutation-string/submission-0.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+        int n = s1.length();
+        int m = s2.length();
+
+        if (n > m) return false;
+
+        int[] freq1 = new int[26];
+        int[] freq2 = new int[26];
+
+        for (int i = 0; i < n; i++) {
+            freq1[s1.charAt(i) - 'a']++;
+            freq2[s2.charAt(i) - 'a']++;
+        }
+
+        if (Arrays.equals(freq1, freq2))
+            return true;
+
+        for (int i = n; i < m; i++) {
+            freq2[s2.charAt(i) - 'a']++;
+            freq2[s2.charAt(i - n) - 'a']--;
+
+            if (Arrays.equals(freq1, freq2))
+                return true;
+        }
+
+        return false;
+    }
+}
