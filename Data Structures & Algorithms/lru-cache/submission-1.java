ff89d14fb7a1af831c1442a640439fd85f241098
vedantnigam0104 <105799215+vedantnigam0104@users.noreply.github.com>
2026-06-27 13:13:32 +0530

    Add: lru-cache - submission-1

---
+++ Data Structures & Algorithms/lru-cache/submission-1.java
@@ -0,0 +1,24 @@
+class LRUCache {
+
+    private int capacity;
+    private LinkedHashMap<Integer, Integer> map;
+
+    public LRUCache(int capacity) {
+        this.capacity = capacity;
+
+        map = new LinkedHashMap<Integer, Integer>(capacity, 0.75f, true) {
+            @Override
+            protected boolean removeEldestEntry(Map.Entry<Integer, Integer> eldest) {
+                return size() > LRUCache.this.capacity;
+            }
+        };
+    }
+
+    public int get(int key) {
+        return map.getOrDefault(key, -1);
+    }
+
+    public void put(int key, int value) {
+        map.put(key, value);
+    }
+}
