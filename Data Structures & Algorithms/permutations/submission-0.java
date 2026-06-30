e77cda9947b2b9670075b4b17e16ff21744b9e21
vedantnigam0104 <105799215+vedantnigam0104@users.noreply.github.com>
2026-06-30 11:01:48 +0530

    Add: permutations - submission-0

---
+++ Data Structures & Algorithms/permutations/submission-0.java
@@ -0,0 +1,24 @@
+public class Solution {
+    List<List<Integer>> res;
+    public List<List<Integer>> permute(int[] nums) {
+        res = new ArrayList<>();
+        backtrack(new ArrayList<>(), nums, new boolean[nums.length]);
+        return res;
+    }
+
+    public void backtrack(List<Integer> perm, int[] nums, boolean[] pick) {
+        if (perm.size() == nums.length) {
+            res.add(new ArrayList<>(perm));
+            return;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (!pick[i]) {
+                perm.add(nums[i]);
+                pick[i] = true;
+                backtrack(perm, nums, pick);
+                perm.remove(perm.size() - 1);
+                pick[i] = false;
+            }
+        }
+    }
+}
