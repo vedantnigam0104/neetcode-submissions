37003893fdaa85c9d3f9b92fa37c2b5783d52f56
vedantnigam0104 <105799215+vedantnigam0104@users.noreply.github.com>
2026-07-09 12:09:41 +0530

    Add: partition-labels - submission-0

---
+++ Data Structures & Algorithms/partition-labels/submission-0.java
@@ -0,0 +1,21 @@
+public class Solution {
+    public List<Integer> partitionLabels(String s) {
+        Map<Character, Integer> lastIndex = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            lastIndex.put(s.charAt(i), i);
+        }
+
+        List<Integer> res = new ArrayList<>();
+        int size = 0, end = 0;
+        for (int i = 0; i < s.length(); i++) {
+            size++;
+            end = Math.max(end, lastIndex.get(s.charAt(i)));
+
+            if (i == end) {
+                res.add(size);
+                size = 0;
+            }
+        }
+        return res;
+    }
+}
